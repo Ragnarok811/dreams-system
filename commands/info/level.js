b204930f6133b8915b6e40b9b624eb6d2ee4b46c
Ragnarok811 <67404559+Ragnarok811@users.noreply.github.com>
2020-06-25 11:22:37 +0700

    Update level.js

--- commands/info/level.js
+++ commands/info/level.js
@@ -4,18 +4,18 @@ const { getInfo } = require("../../handlers/xp.js")
 module.exports = {
   name: "level",
   aliases: ["lv"],
-  description: "Get the level of author or mentioned",
+  description: "Dapatkan Level Pengguna Atau Yang Disebutkan",
   usage: "level <user>",
   category: "info",
   run: (client, message, args) => {
     const user = message.mentions.users.first() || message.author;
     
     if(user.id === client.user.id) { //IF BOT
-      return message.channel.send("😉 | I am on level 100")
+      return message.channel.send("😉 |  Saya Sudah Level 100")
     }
     
     if(user.bot) {
-      return message.channel.send("Bot do not have levels")
+      return message.channel.send("Bot Tydack Punya Level Cuk")
     }
     
     let xp = db.get(`xp_${user.id}_${message.guild.id}`) || 0;
@@ -27,7 +27,7 @@ module.exports = {
     .setAuthor(user.username, message.guild.iconURL())
     .setColor("#ff2050")
     .setThumbnail(user.avatarURL())
-    .setDescription(`**LEVEL** - ${level}
+    .setDescription(`**Tingkat** - ${level}
 **XP** - ${remxp}/${levelxp}`)
     
  message.channel.send(embed)   
