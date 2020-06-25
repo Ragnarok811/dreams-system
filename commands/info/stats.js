7e32598430a3b0a95f53db0edd71196f3e6d3b6d
Rikka Takanashi <64631783+InfinityProject123@users.noreply.github.com>
2020-06-25 11:18:14 +0700

    Update stats.js

--- commands/info/stats.js
+++ commands/info/stats.js
@@ -11,14 +11,11 @@ module.exports = {
       .setTitle(`Stats ${client.user.username}`)
       .setThumbnail(client.user.avatarURL())
       .addField("Server", "Weebsology")
-      .addField("TOTAL MEMBERS", client.users.cache.size)
-      .addField("STATUS", client.user.presence.status, true)
-      .addField("UPTIME", client.uptime, true)
-      .addField("PRESENCE", client.user.presence.activities[0].name, true)
+      .addField("Total Member", client.users.cache.size)
+      .addField("Status", client.user.presence.status, true)
+      .addField("Uptime", client.uptime, true)
+      .addField("Presence", client.user.presence.activities[0].name, true)
       .addField("Owner Bot", "Rikka#4606")
-      .addField("Support Server", `[Click Here](https://discord.gg/TTH8bbz)` , true)
-      .addField("Invite Bot", `[Click Here](https://discordapp.com/oauth2/authorize?client_id=725276690439340033&scope=bot&permissions=2146958847)` , true)
-      .addField("Donate Me", `[Click Here](https://saweria.co/nekodiary)` , true)
       .setFooter(`Created By Chocola#4606`)
     message.channel.send(statsembed);
     }
