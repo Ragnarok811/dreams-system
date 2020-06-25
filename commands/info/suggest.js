60a8fd58005fdee0033655e5532368f5029ddfad
Rikka Takanashi <64631783+InfinityProject123@users.noreply.github.com>
2020-06-25 11:11:18 +0700

    Update suggest.js

--- commands/info/suggest.js
+++ commands/info/suggest.js
@@ -4,24 +4,24 @@ const { MessageEmbed } = require("discord.js");
 module.exports = {
     name: "suggest",
     category: "info",
-    description: "Returns all commands, or one specific command info",
+    description: "Mengirimkan Opini Anda",
     usage: "suggest <message>",
     run: async (client, message, args) => {
 
       if(!args.length) {
-        return message.channel.send("Please Give The Suggestion")
+        return message.channel.send("Tolong Berikan Saran")
       }
 
       let channel = message.guild.channels.cache.find((x) => (x.name === "🚬┊rapat-room" || x.name === "🚬┊rapat-room"))
 
 
       if(!channel) {
-        return message.channel.send("there is no channel with name - suggestion")
+        return message.channel.send("Tidak Ada Channel Bernama Suggestion")
       }
 
 
       let embed = new MessageEmbed()
-      .setAuthor("SUGGESTION: " + message.author.tag, message.author.avatarURL())
+      .setAuthor("SARAN: " + message.author.tag, message.author.avatarURL())
       .setThumbnail(message.author.avatarURL())
       .setColor('RANDOM')
       .setDescription(args.join(" "))
